cf34f155f8989a64f3b372752dcda1ca0f624700
DANI05132 <weroarmendariz123@gmail.com>
2025-09-23 21:47:48 -0700

    Ej9

---
+++ Ej9/Ej9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program {
+    static void SelectionSort(int[] arr) {
+        for (int i = 0; i < arr.Length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.Length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void Main() {
+        Console.Write("Cuantos numeros vas a ingresar? ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        Console.WriteLine("Ingresa los " + n + " numeros:");
+        for (int i = 0; i < n; i++)
+            arr[i] = int.Parse(Console.ReadLine());
+
+        SelectionSort(arr);
+
+        Console.Write("Arreglo ordenado: ");
+        foreach (int num in arr)
+            Console.Write(num + " ");
+    }
+}
